eadfab21a06d8ba5df7f495b69dceb74bffda775
Andrew Sellers <andrew.sellers@stinkstudios.com>
2018-07-11 15:42:02 +0100

    Typography

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 // Components
 import Artwork from './components/artwork';
+import Typography from './components/typography';
 
 import './app.css';
 import tempArtwork from './components/artwork/temp@2x.jpg';
@@ -10,7 +11,28 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        <Typography variant="sub-header" component="h4" margin="mb30">
+          Stink Studios
+        </Typography>
+        <Typography variant="header" component="h3" margin="mb130">
+          Currently Playing
+        </Typography>
         <Artwork imgSrc={tempArtwork} />
+        <Typography variant="header" component="h1" margin="mb30">
+          Time to Dance - Sebatian Remix
+        </Typography>
+        <Typography component="h2" margin="mb200">
+          The Shoes
+        </Typography>
+        <Typography variant="sub-header" component="h5" margin="mb80">
+          Up Next
+        </Typography>
+        <Typography component="h3" margin="mb10">
+          Genesis
+        </Typography>
+        <Typography variant="alt-body" component="h4">
+          Justice
+        </Typography>
       </div>
     );
   }
---
+++ frontend/src/components/typography/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import cx from 'classnames';
+import { oneOf, oneOfType, node, string, func, shape } from 'prop-types';
+
+import classes from './style.module.css';
+
+const Typography = ({
+  children,
+  component: componentProp,
+  headlineMapping,
+  variant,
+  margin,
+  className: additionalClassses,
+  ...other
+}) => {
+  const className = cx(
+    classes.base,
+    {
+      [classes[variant]]: variant,
+      [classes[margin]]: margin,
+    },
+    additionalClassses
+  );
+  const Component = componentProp || headlineMapping[variant] || 'span';
+
+  return (
+    <Component className={className} {...other}>
+      {children}
+    </Component>
+  );
+};
+
+Typography.propTypes = {
+  children: node.isRequired,
+  margin: oneOf([false, 'mb10', 'mb30', 'mb80', 'mb130', 'mb200']),
+  component: oneOfType([string, func]),
+  headlineMapping: shape({
+    body: string,
+    header: string,
+    subHeader: string,
+  }),
+  variant: oneOf(['body', 'alt-body', 'header', 'sub-header']),
+  className: string,
+};
+
+Typography.defaultProps = {
+  margin: false,
+  component: null,
+  headlineMapping: {
+    body: 'p',
+    header: 'h1',
+    'sub-header': 'h2',
+  },
+  variant: 'body',
+  className: '',
+};
+
+export default Typography;
