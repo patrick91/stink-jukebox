1cec285cb13957fd3ca4ca1120798a6de6bdc3d9
Andrew Sellers <andrew.sellers@stinkstudios.com>
2018-07-11 12:59:20 +0100

    Container setup

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import 'normalize.css';
 import './app.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="app">
-        <header className="app__header">
-          <img src={logo} className="app__logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="app__link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="app">Stink Jukebox</div>;
   }
 }
 
