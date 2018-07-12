7388abdef5ef05b1f01584953995ce768d9930c5
Andrew Sellers <andrew.sellers@stinkstudios.com>
2018-07-12 14:29:03 +0100

    Header icon

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -5,6 +5,9 @@ import classes from './style.module.css';
 const Header = () => (
   <header className={classes.header}>
     <span className={classes.logo} />
+    <span className={classes.icon}>
+      <span className={classes.iconInner} />
+    </span>
   </header>
 );
 
