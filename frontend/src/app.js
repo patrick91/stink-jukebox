6da06ac22dc71ec62320fa222d28e5ef4732023d
Andrew Sellers <andrew.sellers@stinkstudios.com>
2018-07-13 14:56:05 +0100

    hooked up to real data

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,69 +1,78 @@
 import React, { Component } from 'react';
+import { ApolloProvider, Subscription } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { client } from './client';
 
 // Components
 import Artwork from './components/artwork';
 import PlaybackBar from './components/playback-bar';
 import Typography from './components/typography';
 import Header from './components/header';
-import UpNext from './components/up-next';
+// import UpNext from './components/up-next';
 import SongMeta from './components/song-meta';
 
 import './app.css';
-import tempArtwork from './components/artwork/temp@2x.jpg';
-// import tempArtwork2 from './components/artwork/temp2.jpg';
+
+const SONG_UPDATED = gql`
+  subscription {
+    onSongUpdated {
+      title
+      artist
+      artworkUrl
+      duration
+      elapsed
+    }
+  }
+`;
 
 class App extends Component {
   state = {
     dark: false,
     online: true,
-    current: {
-      artwork: tempArtwork,
-      artist: 'The Shoes',
-      song: 'Time to Dance - Sebatian Remix',
-      time: {
-        total: 240,
-        elapsed: 100,
-      },
-    },
-    next: {
-      artist: 'Justice',
-      song: 'Genesis',
-    },
   };
 
   componentDidMount() {
     if (this.state.dark) document.body.classList.add('dark');
-    // setTimeout(() => {
-    //   const current = { ...this.state.current };
-    //   current.artwork = tempArtwork2;
-    //   this.setState({ current });
-    // }, 4000);
   }
 
   render() {
-    const { online, current, next } = this.state;
+    const { online } = this.state;
     return (
-      <div className="app">
-        <Header online={online} />
+      <ApolloProvider client={client}>
+        <Subscription subscription={SONG_UPDATED}>
+          {({ data, error, loading }) => {
+            if (loading || error) return null;
+            const {
+              title,
+              artist,
+              artworkUrl,
+              duration,
+              elapsed,
+            } = data.onSongUpdated;
+            return (
+              <div className="app">
+                <Header online={online} />
 
-        <Typography variant="sub-header" component="h4" margin="mb30">
-          Stink Studios
-        </Typography>
-        <Typography variant="header" component="h3" margin="mb80">
-          {online ? 'Currently Playing' : 'Offline'}
-        </Typography>
+                <Typography variant="sub-header" component="h4" margin="mb30">
+                  Stink Studios
+                </Typography>
+                <Typography variant="header" component="h3" margin="mb80">
+                  {online ? 'Currently Playing' : 'Offline'}
+                </Typography>
 
-        <Artwork imgSrc={current.artwork} />
+                <Artwork imgSrc={artworkUrl} />
 
-        <PlaybackBar
-          totalTime={current.time.total}
-          elapsedTime={current.time.elapsed}
-        />
+                <PlaybackBar totalTime={duration} elapsedTime={elapsed} />
 
-        <SongMeta song={current.song} artist={current.artist} />
+                <SongMeta song={title} artist={artist} />
 
-        <UpNext song={next.song} artist={next.artist} />
-      </div>
+                {/* <UpNext song={next.song} artist={next.artist} /> */}
+              </div>
+            );
+          }}
+        </Subscription>
+      </ApolloProvider>
     );
   }
 }
